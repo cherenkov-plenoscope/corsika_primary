bef964a9bb962bf03cd710c53e4f8cd928b2e7fd
sebastian <sebmuell@phys.ethz.ch>
2016-11-20 22:10:23 +0100

    fix wrong doc string

--- custom_corsika/__init__.py
+++ custom_corsika/__init__.py
@@ -1,7 +1,7 @@
 """
 Install a custom CORSIKA for the Atmospheric Cherenkov Plenoscope (ACP)
 
-Usage: install_corsika_acp -p=INSTALL_PATH --username=USERNAME --password=PASSWORD
+Usage: custom_corsika -p=INSTALL_PATH --username=USERNAME --password=PASSWORD
 
 Options:
     -p --path=INSTALL_PATH      The path to install CORSIKA in
