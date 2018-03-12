f748abe76877eee696f0029f7a666b4f824286c0
sebastian <sebmuell@phys.ethz.ch>
2018-03-12 16:39:34 +0100

    rm obsolete files

--- acp_corsika_install/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-A package to install the CORSIKA air-shower simulaton for investigations of the
-Atmospheric Cherenkov Plenoscope (ACP).
-"""
-from . import tools
-from ._api import install
--- install.py
+++ install.py
@@ -7,6 +7,24 @@ import glob
 from . import tools
 
 
+def call_and_save_std(target, stdout_path, stderr_path, stdin=None):
+    """
+    Same as subprocess.call, but stores the stdout and stderr to text files.
+
+    Parameters
+    ----------
+    target          The target to be called with subprocess.call().
+
+    stdout_path     The path where the stdout of the target call is written to.
+
+    stderr_path     The path where the stderr of the target call is written to.
+
+    stdin           Optional stdin fot the target.
+    """
+    with open(stdout_path, 'w') as stdout, open(stderr_path, 'w') as stderr:
+        subprocess.call(target, stdout=stdout, stderr=stderr, stdin=stdin)
+
+
 def install(install_path, username, password):
     """
     Installs the KIT CORSIKA for the Atmospheric Cherenkov Plenoscope (ACP) into 
