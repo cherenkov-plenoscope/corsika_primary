d89cf48a5d76dd8a42c27f46f5acc6ea0c47963d
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-03-16 12:42:30 +0100

    add comment

--- corsika_primary/calibration_light_source.py
+++ corsika_primary/calibration_light_source.py
@@ -12,6 +12,30 @@ def draw_parallel_and_isochor_bunches(
     prng,
     speed_of_light=299792458,
 ):
+    """
+    Returns photon-bunches emitted by a star-like source of light.
+    The photons are meant to arrive in the same pixel in the same moment.
+    This means, they travel in a plane perpendicular to their direction
+    of motion.
+
+    Parameters
+    ----------
+    cx : float
+        Angle of incident w.r.t. x-axis in rad.
+    cy : float
+        Angle of incident w.r.t. y-axis in rad.
+    aperture_radius : float >= 0
+        Radius of the disc illuminated by the photons on the aperture's
+        principal plane a.k.a. observation-level.
+    wavelength : float >= 0
+        The wavelength of the photon-bunches.
+    size : int >= 0
+        The number of photon-bunches.
+    prng : numpy.random.Generator
+
+    speed_of_light : float
+        The photon's speed right above the aperture / obersavtion-level.
+    """
     assert aperture_radius >= 0
     assert wavelength >= 0
     assert size >= 0
