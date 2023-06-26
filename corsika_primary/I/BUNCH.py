d54b1965ed84031e81cd24f444aa2d9ef39e5a23
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-06-26 20:55:42 +0200

    add explicit units to index-markers

--- corsika_primary/I/BUNCH.py
+++ corsika_primary/I/BUNCH.py
@@ -1,3 +1,8 @@
+NUM_FLOAT32 = 8
+NUM_BYTES = NUM_FLOAT32 * 4
+
+# short to be discontinued
+# ------------------------
 X = 0
 Y = 1
 CX = 2
@@ -6,5 +11,14 @@ TIME = 4
 ZEM = 5
 BSIZE = 6
 WVL = 7
-NUM_FLOAT32 = 8
-NUM_BYTES = NUM_FLOAT32 * 4
+
+# explicit units
+# --------------
+X_CM = 0
+Y_CM = 1
+CX_RAD = 2
+CY_RAD = 3
+TIME_NS = 4
+EMISSOION_ALTITUDE_ASL_CM = 5
+BUNCH_SIZE_1 = 6
+WAVELENGTH_NM = 7
