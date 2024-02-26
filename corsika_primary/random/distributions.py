7f5ee5cf2321ba92a10b602de3838068b744b5dc
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-02-26 12:41:28 +0100

    Add doc strings

--- corsika_primary/random/distributions.py
+++ corsika_primary/random/distributions.py
@@ -2,6 +2,26 @@ import numpy as np
 
 
 def draw_power_law(prng, lower_limit, upper_limit, power_slope, num_samples):
+    """
+    Parameters
+    ----------
+    prng : numpy.random.Generator
+        Pseudo random number generator
+    lower_limit : float
+        Lower limit of the distribution to draw from.
+    upper_limit : float
+        Upper limit of the distribution to draw from.
+    power_slope : float
+        Slope of the power law to draw from.
+    num_samples : int
+        The number of samples to be drawn.
+
+    Returns
+    -------
+    x : numpy.array, floats
+        Values between 'lower_limit' and 'upper_limit' occuring a ccording to
+        a power law with slope 'power_slope'.
+    """
     # Adopted from CORSIKA
     rd = prng.uniform(size=num_samples)
     if power_slope != -1.0:
@@ -32,6 +52,32 @@ def draw_azimuth_zenith_in_viewcone(
     max_zenith_rad=np.deg2rad(70),
     max_iterations=1000 * 1000,
 ):
+    """
+    Draw a random pointing (azimuth, zenith distance) from within a viewcone.
+
+    Parameters
+    ----------
+    prng : numpy.random.Generator
+        Pseudo random number generator
+    azimuth_rad : float
+        Azimuth pointing of viewcone.
+    zenith_rad : float
+        Zenith distance pointing of viewcone.
+    min_scatter_opening_angle_rad : float
+        Minimum half angle of viewcone to draw from.
+    max_scatter_opening_angle_rad : float
+        Maximum half angle of viewcone to draw from.
+    max_zenith_rad : float
+        The max. zenith distance a drawn pointing must have.
+    max_iterations : int
+        Limiting the max zenith distance is done with rejection sampling.
+        This max_iterations limit the max. number of rejections.
+
+    Returns
+    -------
+    (azimuth, zenith distance) : (float, float)
+        In rad.
+    """
     assert min_scatter_opening_angle_rad >= 0.0
     assert max_scatter_opening_angle_rad >= min_scatter_opening_angle_rad
     assert max_zenith_rad >= 0.0
@@ -71,6 +117,22 @@ def draw_azimuth_zenith_in_viewcone(
 
 
 def draw_x_y_in_disc(prng, radius):
+    """
+    Draw a random position within a disc.
+
+    Parameters
+    ----------
+    prng : numpy.random.Generator
+        Pseudo random number generator
+    radius : float
+        Radius of the disc.
+
+    Returns
+    -------
+    (x, y) : (float, float)
+        A random position on the xy plane within a radius 'radius' from the
+        origin.
+    """
     rho = np.sqrt(prng.uniform(low=0.0, high=1.0)) * radius
     phi = prng.uniform(low=0.0, high=2.0 * np.pi)
     x = rho * np.cos(phi)
