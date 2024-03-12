cf8cd1127310dee053b6f80102fccde48f3b689c
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-03-12 13:04:29 +0100

    adopt random points from sphere to package spherical coordinates

--- corsika_primary/random/distributions.py
+++ corsika_primary/random/distributions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import spherical_coordinates
 
 
 def draw_power_law(prng, lower_limit, upper_limit, power_slope, num_samples):
@@ -81,38 +82,22 @@ def draw_azimuth_zenith_in_viewcone(
     assert min_scatter_opening_angle_rad >= 0.0
     assert max_scatter_opening_angle_rad >= min_scatter_opening_angle_rad
     assert max_zenith_rad >= 0.0
-    # Adopted from CORSIKA
+
     zenith_too_large = True
     iteration = 0
     while zenith_too_large:
-        rd1, rd2 = prng.uniform(size=2)
-        ct1 = np.cos(min_scatter_opening_angle_rad)
-        ct2 = np.cos(max_scatter_opening_angle_rad)
-        ctt = rd2 * (ct2 - ct1) + ct1
-        theta = np.arccos(ctt)
-        phi = rd1 * np.pi * 2.0
-        # TEMPORARY CARTESIAN COORDINATES
-        xvc1 = np.cos(phi) * np.sin(theta)
-        yvc1 = np.sin(phi) * np.sin(theta)
-        zvc1 = np.cos(theta)
-        # ROTATE AROUND Y AXIS
-        xvc2 = xvc1 * np.cos(zenith_rad) + zvc1 * np.sin(zenith_rad)
-        yvc2 = yvc1
-        zvc2 = zvc1 * np.cos(zenith_rad) - xvc1 * np.sin(zenith_rad)
-        zd = np.arccos(zvc2)
+        az, zd = spherical_coordinates.random.uniform_az_zd_in_cone(
+            prng=prng,
+            azimuth_rad=azimuth_rad,
+            zenith_rad=zenith_rad,
+            min_half_angle_rad=min_scatter_opening_angle_rad,
+            max_half_angle_rad=max_scatter_opening_angle_rad,
+        )
         if zd <= max_zenith_rad:
             zenith_too_large = False
         iteration += 1
         if iteration > max_iterations:
             raise RuntimeError("Rejection-sampling failed.")
-    if xvc2 != 0.0 or yvc2 != 0.0:
-        az = np.arctan2(yvc2, xvc2) + azimuth_rad
-    else:
-        az = azimuth_rad
-    if az >= np.pi * 2.0:
-        az -= np.pi * 2.0
-    if az < 0.0:
-        az += np.pi * 2.0
     return az, zd
 
 
